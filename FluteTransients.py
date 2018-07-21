14f8aee6cb705202d792c5bf4e62ed9e927750dd
adigitoleo <34595875+adigitoleo@users.noreply.github.com>
2018-07-21 23:16:26 +1000

    final commit, project report submitted

--- FluteTransients.py
+++ FluteTransients.py
@@ -2,9 +2,10 @@ import numpy as np
 import TransferFunctions as tf
 import scipy.signal as sig
 import audacity
+import peakutils
 
 ###-+-+-+-+-+
-### Functions for TF analysis method
+### Functions for TF resonance analysis
 ###-+-+-+-+ 
 
 def get_tfdata(aupfile, *args, nfft, testing=False):
@@ -130,7 +131,7 @@ def get_hf(tfdata, fvals, lflim, uflim):
     return hfx, hfy
 
 ###-+-+-+-+-+
-### Functions for F0 analysis method
+### Functions for static geometry frequency analysis
 ###-+-+-+-+ 
 
 def get_fsdata(aupfile, *args, nfft=1024):
@@ -236,7 +237,124 @@ def get_f0(wfdata, lflim, uflim, nfft=1024, sr=44100):
     return f0x, f0y
 
 ###-+-+-+-+-+
-### Other useful functions
+### Functions for live transient analysis
+###-+-+-+-+ 
+
+def IRcalib(A, coeff):
+    """
+    A must be a numpy array
+    """
+    (a,b,c) = coeff
+    gradIR = np.gradient(A)
+    keyx = (np.sqrt(b**2-4*a*(c-A))-b)/(2*a)*(2.5e-04)
+    gradX = 1/(np.sqrt(b**2-4*a*(c-A)))
+    keyv = gradX*gradIR
+    
+    return keyx,keyv
+
+def Pcalib(A, coeff):
+    """
+    A must be a numpy array
+    """
+    (a,b) = coeff
+    P = a*A+b
+    return P
+
+def get_vpeaks(v, t, thresh, minsep, pk='max'):
+    """
+    """
+    vabs = np.abs(v)
+    i_bad = [ i for i, tval in enumerate(t)
+             if tval < 1. or tval > t[-1]-1. ]
+    vabs[i_bad] = 0
+    peaks = peakutils.indexes(vabs, thres=thresh, min_dist=minsep)
+    
+    if pk=='max':
+        vpeaks = [ v[i] for i in peaks if v[i] > 0]
+        tpeaks = [ t[i] for i in peaks if v[i] > 0]
+    elif pk=='min':
+        vpeaks = [ v[i] for i in peaks if v[i] < 0]
+        tpeaks = [ t[i] for i in peaks if v[i] < 0]
+    else:
+        raise ValueError(
+            "Invalid peak type. Expected 'max' or 'min'")
+    return vpeaks,tpeaks
+
+def get_fpeaks(au, t, sr, est, sweep, opt=False):
+    """
+    """
+    i_est = [ int(round(tval*sr)) for tval in est ]
+    nsweep = int(round(sweep*sr))
+    sweeplims = ([ indx-nsweep for indx in i_est ],
+                 [ indx+nsweep for indx in i_est ])
+    
+    gradpeaks = []
+    timepeaks = []
+    audio = []
+    zerox = []
+    freqs = []
+    for this, (start, stop) in enumerate(zip(sweeplims[0], sweeplims[1])):
+        this_audio = au[start:stop]
+        this_time = t[start:stop]
+        
+        # au[start+i-1] is the prev. sample
+        # automatically skips half-cycle crosses
+        i_zerocross = [ i for i, sample in enumerate(this_audio)
+                       if sample > 0 and au[start+i-1] < 0 ]
+        single_periods = [ this_time[i_zerocross[i]]-this_time[indx]
+                          for i, indx in enumerate(i_zerocross, 1)
+                         if i != (len(i_zerocross)) ]
+        cycle_freqs = [ 1/period for period in single_periods ]
+        
+        # lowpass filter freqs. to remove sampling artefacts
+        b, a = sig.butter(4, 1/8, 'low')
+        filtfreq = sig.filtfilt(b, a, cycle_freqs, padtype='even')
+        
+        gradf = np.abs(np.gradient(filtfreq))
+        gradf_peakval = np.max(gradf)
+        
+        # no check for multiple peaks, gradf_timeval *could* have len > 1
+        gradf_timeval = [ this_time[i] for i, df in enumerate(gradf)
+                         if df==gradf_peakval ]
+        audio.append(this_audio)
+        zerox.append(i_zerocross)
+        freqs.append(cycle_freqs)
+        gradpeaks.append(gradf_peakval)
+        timepeaks.append(gradf_timeval)
+    
+    if opt==True:
+        return audio, zerox, freqs
+    else:
+        timepeaks = np.unique(timepeaks).tolist()
+        return gradpeaks, timepeaks
+    
+def get_ppeaks(au, t, sr, est, sweep):
+    """
+    Requires RMSWind
+    """
+    i_est = [ int(round(tval*sr)) for tval in est ]
+    nsweep = int(round(sweep*sr))
+    sweeplims = ([ indx-nsweep for indx in i_est ],
+                 [ indx+nsweep for indx in i_est ])
+    
+    gradpeaks = []
+    timepeaks = []
+    for this, (start, stop) in enumerate(zip(sweeplims[0], sweeplims[1])):
+        this_audio = au[start:stop]
+        this_time = t[start:stop]
+        pfilt, ptime = RMSWind(this_audio, sr=sr, nwind=512, nhop=256)
+        # do i need to unpack ptime here?
+        
+        pgrad = np.abs(np.gradient(pfilt))
+        pgrad_peakval = np.max(pgrad)
+        pgrad_timeval = [ this_time[i] for i, dp in enumerate(pgrad)
+                         if dp==pgrad_peakval ]
+        gradpeaks.append(pgrad_peakval)
+        timepeaks.append(pgrad_timeval)
+    return gradpeaks, timepeaks
+    
+###-+-+-+-+-+
+### Other useful functions/wrappers
 ###-+-+-+-+ 
 
 def get_stdevs(data, meandata):
@@ -251,3 +369,34 @@ def get_stdevs(data, meandata):
     sumsqs = [sum(x) for x in zip(*acqdevs)]
     stdevs = [np.sqrt(x/(len(data)-1)) for x in sumsqs]
     return np.array(stdevs)
+
+# Andre - windowed RMS calculation
+def RMSWind(x, sr=1, nwind=1024, nhop=512, windfunc=np.blackman):
+    '''
+    Calculates the RMS amplitude amplitude of x, in frames of
+    length nwind, and in steps of nhop. windfunc is used as
+    windowing function.
+    nwind should be at least 3 periods if the signal is periodic.
+    '''
+
+    nsam = len(x)
+    ist = 0
+    iend = ist+nwind
+
+    t = []
+    ret = []
+
+    wind = windfunc(nwind)
+    wsum2 = np.sum(wind**2)
+
+    while (iend < nsam):
+        thisx = x[ist:iend]
+        xw = thisx*wind
+
+        ret.append(np.sum(xw*xw/wsum2))
+        t.append(float(ist+iend)/2.0/float(sr))
+
+        ist = ist+nhop
+        iend = ist+nwind
+
+    return np.sqrt(np.array(ret)), np.array(t)
